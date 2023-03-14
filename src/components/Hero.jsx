25d55edff6ccd5a127c5b088a581d6dfaba10d44
obedgyamfi <obedgyamfi53@gmail.com>
2023-03-14 16:46:05 +0000

    hero section static design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <div className="bg-blue-400 w-auto h-screen">
+      <div class='inline-block justify-center items-center m-10 p-10'>
+        <h1 class=" font-extrabold text-9xl m-4">Revision</h1>
+        <p class='text-3xl m-5'>Practice questions and be graded in the comfort of your bed</p>
+        <button class='bg-black p-4 m-5 items-center'>Get Started</button>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
-            <main className="bg-black h-64 font-nunito text-white">
+            <main className="font-nunito text-white">
             <Navbar />
             <Hero />
             </main>
