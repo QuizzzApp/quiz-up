a52d7c9697c3de1e1f1a60c925e7af77f691bd1a
blvckwidow <blvckwidow@blvckwidow>
2023-04-03 00:19:21 +0000

    obed commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/Link";
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-center md:justify-end bg-blue-400 text-white w-full h-15 p-3 px-20">
+      <Link id="link" href="/" className="flex items-center w-auto p-2 px-10">
+        <svg
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          className="fill-current text-white h-8 w-8"
+        >
+          <path d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z" />
+        </svg>
+        <span className="flex text-2xl text-white font-bold tracking-wide p-2 w-auto">
+          QuizUp
+        </span>
+      </Link>
+    </nav>
+  );
+};
+
+export default Navbar;
