026481b2409b8692e0279de937e2ce40b4806c96
obedgyamfi <obedgyamfi53@gmail.com>
2023-06-09 03:33:24 +0000

    commit: all errors fixed

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Hero = () => {
   return (
-    <div className="h-screen flex-grow items-center bg-blue-400 w-auto">
+    <div className="h-screen flex-grow items-center bg-blue-400 overflow y-auto w-auto">
       <div className='inline-block justify-center items-center md:m-10 p-10'>
         <h1 className=" font-extrabold text-5xl md:text-9xl my-4 md:m-4">QuizUp</h1>
         <p className='md:text-3xl text-xl mx-auto my-auto md:m-5'>Practice questions and be graded in the comfort of your bed</p>
